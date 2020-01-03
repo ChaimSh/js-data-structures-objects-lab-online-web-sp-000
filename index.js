4f8944dd6ea83b77e867e5a0c9ec7853dd29faa7
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-01-03 00:37:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
 function deleteFromDriverByKey(driver, key) {
   const newObj = Object.assign({}, driver);
 
-  delete newObj.keys();
+  delete Object.keys(newObj);
 
    newObj;
 }
